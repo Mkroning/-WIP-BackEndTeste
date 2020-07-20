aedf7e01ce2f42f8f890236b36cbd099b4672088
Matheus Kroning <kroningmatheus@gmail.com>
2020-07-19 23:11:52 -0300

    Arrumando routes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,7 +4,7 @@ import { createUser, singUp } from '../controllers/userController';
 
 const router = express.Router();
 
-router.post('/auth/signup', createUser);
+router.post('/auth/create', createUser);
 router.post('/auth/signin', singUp);
 
 export default router;
